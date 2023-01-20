bb486be98fcd92419c3697d3682a44d1ef896cb5
Juan Amieva <juandejunin75@gmail.com>
2023-01-20 20:15:11 +0100

    feat: add env

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 import Sequelize from 'sequelize'
+import dotenv from 'dotenv'
+dotenv.config({path:'.env'})
 
-const db = new Sequelize('inmobiliaria','root','1234',{
-    host: 'localhost',
+const db = new Sequelize(process.env.DB_NOMBRE,process.env.DB_USER,process.env.DB_PASS,{
+    host:process.env.DB_HOST,
     port: 3306,
     dialect:'mysql',
     define:{
